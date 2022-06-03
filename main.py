f3cbfd02f16b6ea954b99f5124c20c1180fe3aa2
Pavel Degtyarev <pvldgt@gmail.com>
2022-06-03 14:48:25 +0300

    add Twilio functionality and add the comments

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
+from twilio.rest import Client
+from credentials import STOCK_API_KEY, NEWS_API_KEY, \
+    twilio_account_sid, twilio_auth_token, my_number, twilio_number
 
-from credentials import STOCK_API_KEY, NEWS_API_KEY
+STOCK = "PYPL"
+COMPANY_NAME = "Paypal"
 
-STOCK = "AMZN"
-COMPANY_NAME = "Amazon"
-
-## GET THE STOCK INFO
-# connect to the API
+# get the stock info by connecting to the API
 stock_parameters = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK,
@@ -14,7 +14,7 @@ stock_parameters = {
 }
 data = requests.get(url="https://www.alphavantage.co/query", params=stock_parameters)
 stock_info = data.json()
-print(stock_info)
+
 # make it possible to fetch the closing values of the 2 previous trading days
 values = stock_info["Time Series (Daily)"].values()
 iter_object = iter(values)
@@ -23,68 +23,63 @@ previous_trading_day = float(next(iter_object)["4. close"])
 # get the closing value of the day before previous trading day
 pre_previous_trading_day = float(next(iter_object)["4. close"])
 
-# print(type(previous_trading_day))
-# print(previous_trading_day)
-# print(type(pre_previous_trading_day))
-# print(pre_previous_trading_day)
-
 # get the difference between 2 closes in %
 difference = abs(previous_trading_day - pre_previous_trading_day)
 # print(difference)
 percent_difference = round(difference * 100 / previous_trading_day, 1)
-# print(percent_difference)
 
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+# When the stock price increases/decreases by 5% between yesterday and the day before yesterday then return True
 def major_event():
     if percent_difference >= 5:
         return True
     else:
         return False
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
-# api setup
-news_parameters = {
-    "q": COMPANY_NAME,
-    "category": "business",
-    "language": "en",
-    "country": "us",
-    "apiKey": NEWS_API_KEY
-}
-data = requests.get(url="https://newsapi.org/v2/top-headlines", params=news_parameters)
-news = data.json()
-print(news)
-
-# create dictionary of top 3 stories
-# top_stories_dict = {}
-# for i in range(0, len(news["articles"])):
-#     top_stories_dict[news["articles"][i]["title"]] = news["articles"][i]["description"]
-
-# if there was a major event, send a test with 3 news pieces
+# if there was a major event, send an SMS alert with 3 news pieces
 if major_event():
+    # grab the 3 top news stories about the stock with the help of newsapi.org
+    news_parameters = {
+        "q": COMPANY_NAME,
+        "category": "business",
+        "language": "en",
+        "country": "us",
+        "apiKey": NEWS_API_KEY
+    }
+    data = requests.get(url="https://newsapi.org/v2/top-headlines", params=news_parameters)
+    news = data.json()
+
     if previous_trading_day - pre_previous_trading_day < 0:
         percent_difference += -1
         emoji = "🔻"
     else:
         emoji = "🟢"
-    if news["totalResults"] == 0:
-        print(f"{STOCK}: {emoji} {percent_difference}")
-    else:
-        for news_item in range (0,3):
-            print(f""""{STOCK}: {emoji} {percent_difference}
-            {news["articles"][news_item]["title"]}
-            {news["articles"][news_item]["description"]}""")
 
-#
-# #Optional: Format the SMS message like this:
-# """
-# TSLA: 🔺2%
-# Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
-# Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-# or
-# "TSLA: 🔻5%
-# Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
-# Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+    # set up the Twilio client
+    client = Client(twilio_account_sid, twilio_auth_token)
+    # if there is no news for the company, then just send the ticker and the percentage point
+    if news["totalResults"] == 0:
+        message = client.messages.create(
+            body=f"{STOCK}: {emoji} {percent_difference}",
+            from_=twilio_number,
+            to=my_number
+        )
+        print(message.status)
+    # otherwise send the 3 news articles too
+    else:
+        try:
+            for news_item in range(0, 3):
+                message = client.messages.create(
+                    body=f"""{STOCK}: {emoji} {percent_difference}
+                    {news["articles"][news_item]["title"]}
+                    {news["articles"][news_item]["description"]}""",
+                    from_=twilio_number,
+                    to=my_number
+                )
+                print(message.status)
+        except IndexError:
+            print("There were fewer than 3 articles")
+# don't send anything if there was no major event
+else:
+    print("There was no major event.")
